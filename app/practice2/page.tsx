69c06b9e6f88ed3454e679b7be3fcba0d15edf32
LingChen-tsjmdlc <3024705530@qq.com>
2025-11-27 10:34:34 +0800

    fix: 强制替换public文件夹部署时路径

--- app/practice2/page.tsx
+++ app/practice2/page.tsx
@@ -4,10 +4,10 @@ import { Image } from "@heroui/react";
 export default function Practice2Page() {
   return (
     <div className="mt-40 px-20">
-      <Image src="/icons/game.png" alt="Game Icon" />
-      <Image src="/icons/chat-bubbles.png" alt="Game Icon" />
-      <Image src="/icons/folder.png" alt="Game Icon" />
-      <Image src="/icons/image.png" alt="Game Icon" />
+      <Image src="/user-interface-design/icons/game.png" alt="Game Icon" />
+      <Image src="/user-interface-design/icons/chat-bubbles.png" alt="Game Icon" />
+      <Image src="/user-interface-design/icons/folder.png" alt="Game Icon" />
+      <Image src="/user-interface-design/icons/image.png" alt="Game Icon" />
     </div>
   );
 }
